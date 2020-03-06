273b9935528a12b0a4f5cf92537480a08c3d3108
Alejandro Alamar <ale.jaam7@gmail.com>
2020-03-05 19:41:51 -0600

    Inicio del proyecto

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Text("Hola mundo"),
+    );
+  }
+}
